814e9da75b68b33c1d0c3dc860e08d601cfcc2bb
Batuto <topoto.toto.2@gmail.com>
2016-09-13 21:23:49 -0500

    Se completó la base para FCFS y SJF(no expulsivo), pero aún necesita revisión

--- simulador.py
+++ simulador.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 #-*-coding:utf-8-*-
 
-# 5 algoritmos
+# Idea del programa:
+# Implementar 5 algoritmos »
 # FCFS/FIFO
 # SJF No expulsivo
 # SJF Expulsivo
@@ -27,15 +28,15 @@ random.seed()
 
 def main():
 	global procesos
-        cantidad_de_procesos = random.randint(1, 35)
+        cantidad_de_procesos = random.randint(2, 35)
 	# Inicializar la lista de procesos
 	for x in range(cantidad_de_procesos):
-		# [numero proceso , tiempo llegada , rafaga de cpu , prioridad ]
-		procesos.append([x, 0, random.randint(2,50), random.randint(0,10)])
+		# [numero proceso , tiempo llegada , rafaga de cpu , prioridad ]i
+		procesos.append([x, 0, random.randint(2,10), random.randint(0,10)])
 	# Meterle los tiempos de llegada sin que se repitan
 	# Compresión de listas | Ver detalles al final del documento
 	#nuevos_tiempos_de_llegada = [ d.setdefault(x,x) for x in [random.randint(0,200) for x in range(cantidad_de_procesos ** 2)] if x not in d ]
-	nuevos_tiempos_de_llegada = [d.setdefault(x,x) for d in [{}] for x in [random.randint(0,100) for y in range(cantidad_de_procesos ** 3)] if x not in d]
+	nuevos_tiempos_de_llegada = [d.setdefault(x,x) for d in [{}] for x in [random.randint(0,50) for y in range(cantidad_de_procesos ** 3)] if x not in d]
 	#[ d.setdefault(x,x) for d in [{}] for x in startList if x not in d ]
 	# Inserta los nuevos tiempos.
 	for x in range(cantidad_de_procesos):
@@ -54,10 +55,10 @@ def main():
 def algoritmo_fifo(respuesta):
     global procesos
     respuesta = respuesta
-    numero_procesos_llegada = []
-    procesos_ordenados_x_llegada = {}
-    procesos_ordenados_x_llegada2 = []
-    lista_nueva = []
+    numero_procesos_llegada = []  # Se inicializan las variables
+    procesos_ordenados_x_llegada = {}  # Se inicializan las variables
+    procesos_ordenados_x_llegada2 = []  # Se inicializan las variables
+    lista_nueva = []  # Se inicializan las variables
     for each_process in procesos:
         numero_procesos_llegada.append(each_process[1])
         procesos_ordenados_x_llegada[each_process[1]] = each_process
@@ -67,13 +68,13 @@ def algoritmo_fifo(respuesta):
         lista_nueva.append(procesos_ordenados_x_llegada[x][:])
     if respuesta:
         acomodo(procesos_ordenados_x_llegada2)  # Si la respuesta es verdadera llama a la función que se encarga de mostrar en pantalla de manera ordenada.
-        print "\nNP\tLL\tRC\tP!\tTRet\tTEsp"
+        print "\nNomP\tLL\tRCpu\tPrior!\tTRet\tTEsp"
     lista_nueva = lista_nueva[:]
     acumulador_tiempo_ret = lista_nueva[0][1]
     acumulador_tiempo_esp = lista_nueva[0][1]
     for x in range(len(lista_nueva)):
-        acumulador_tiempo_ret += lista_nueva[x][2]
-        lista_nueva[x].append(acumulador_tiempo_ret)
+        acumulador_tiempo_ret += lista_nueva[x][2]  # Variable para llevar el total de los tiempos de cpu para cada proceso, así poder obtener los tiempos de retorno de cada uno.
+        lista_nueva[x].append(acumulador_tiempo_ret)  # Se añade a la «matriz» el tiempo de retorno 'en turno'.
         if acumulador_tiempo_esp - lista_nueva[x][1] <= 0:
             acumulador_tiempo_esp = 0
         else:
@@ -85,10 +86,10 @@ def algoritmo_fifo(respuesta):
     return procesos_ordenados_x_llegada2
 
 #export PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games'
-incremental = 0.999
+incremental = 0.999  # Variable usada para poder llevar un mejor control sobre el ordenamiento en donde los tiempos de cpu se repiten.
 def algoritmo_sjf(procesos_ordenados_x_llegada):
     procesos_ordenados_x_llegada = procesos_ordenados_x_llegada
-    procesos_ordenados_x_tiempo_de_rafaga = {}
+    procesos_ordenados_x_tiempo_de_rafaga = {}  # Inicializo el diccionario
     lista_poxtr = []  # Lista con los tiempos de ráfaga. (poxtr = Procesos ordenados x tiempo de ráfaga)
 #    incremental = 0.999
 #    for each_process in procesos_ordenados_x_llegada[1:]:
@@ -111,7 +112,29 @@ def algoritmo_sjf(procesos_ordenados_x_llegada):
     procesos_ordenados_x_tiempo_de_rafaga2 = []
     for x in lista_poxtr:
         procesos_ordenados_x_tiempo_de_rafaga2.append(procesos_ordenados_x_tiempo_de_rafaga[x])
+
     acomodo(procesos_ordenados_x_tiempo_de_rafaga2)
+
+    acumulador_tiempo_ret = procesos_ordenados_x_tiempo_de_rafaga2[0][1]
+    acumulador_tiempo_esp = procesos_ordenados_x_tiempo_de_rafaga2[0][1]
+    for x in range(len(procesos_ordenados_x_tiempo_de_rafaga2)):
+        acumulador_tiempo_ret += procesos_ordenados_x_tiempo_de_rafaga2[x][2]
+        procesos_ordenados_x_tiempo_de_rafaga2[x].append(acumulador_tiempo_ret)
+        if acumulador_tiempo_esp - procesos_ordenados_x_tiempo_de_rafaga2[x][1] <=0:
+            acumulador_tiempo_esp = 0
+        else:
+            acumulador_tiempo_esp -= procesos_ordenados_x_tiempo_de_rafaga2[x][1]
+        procesos_ordenados_x_tiempo_de_rafaga2[x].append(acumulador_tiempo_esp)
+        acumulador_tiempo_esp = acumulador_tiempo_ret
+    # Parche ----
+    for x in range(len(procesos_ordenados_x_tiempo_de_rafaga2)):
+        while len(procesos_ordenados_x_tiempo_de_rafaga2[x]) > 6:
+            procesos_ordenados_x_tiempo_de_rafaga2[x].pop()
+    # -----------
+    print "\nNomP\tLL\tRCpu\tPrior!\tTRet\tTEsp"
+    for u,v,w,x,y,z in procesos_ordenados_x_tiempo_de_rafaga2:
+        print u,"\t",v,"\t",w,"\t",x,"\t",y,"\t",z
+
     """for x in lista_poxtr:
         print x," ",procesos_ordenados_x_tiempo_de_rafaga[x]"""
     """np.sort()
@@ -122,7 +145,7 @@ def algoritmo_sjf(procesos_ordenados_x_llegada):
             print procesos[x].index(u)"""
 def acomodo(lista_procesos):
     # Pequeña función que se encarga de mostrar en pantalla las listas que se pasen como parámetro, añadiendo una tabulaciones para una buena presentación.
-    print "NP\tLL\tRC\tP!"
+    print "NomP\tLL\tRCpu\tPrior!"
     for w,x,y,z in lista_procesos:
         print w,"\t",x,"\t",y,"\t",z
 
@@ -132,7 +155,7 @@ if __name__ == '__main__':
 """
 Notas
 Números son repetición
-
+#
 startList = [5,1,2,1,3,4,2,5,3,4]
 d = {}
 [ d.setdefault(x,x) for x in startList if x not in d ] [5, 1, 2, 3, 4]
